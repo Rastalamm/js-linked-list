ce1505c85fe4d485087ef0c61e5c1147c24aff35
Judah Lamm <Judah.Lamm@gmail.com>
2015-06-02 09:22:31 -1000

    Solution to the remove function

--- linkedList.js
+++ linkedList.js
@@ -56,45 +56,35 @@ function linkedListGenerator(){
       var previous;
       var nextNode;
 
-
       if(num === 0 ){
             head = head.next;
             return head;
       }else{
 
         while (current !== null && count <= num){
-
-            if(count === num){
-              console.log("nextNode here works",nextNode);
-
-              if(current.next === null){
-
-                console.log('nextNode here', nextNode);
-                  previous.next = null;
-                  tail = previous;
-                  console.log('removed', current);
-                  return current;
-
-
+          console.log('enters again?', count);
+          if(count < num){
+            if(nextNode === null){
+              return false;
+              }
+            previous = current; //this is what gives previous
+            current = current.next;
+            nextNode = current.next;// this is what give next
+            count++;
+          }else{
+            if(current.next === null){
+              previous.next = null;
+              tail = previous;
+              return false;
               }
               else{
                 previous.next = nextNode;
-                return previous;
+                return previous.current;
               }
-            }
-            else{
-              previous = current; //this is what gives previous
-              current = current.next;
-              nextNode = current.next;// this is what give next
-              count++;
-            }
-
           }
+        }
         return false;
         }
-
-        console.log('nothign fits');
-
     },
 
     insert : function(){
